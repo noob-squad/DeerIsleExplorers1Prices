8b7731ef721730f29c529e524501a859d81433b0
Volodymyr Sergeyev <volodymyr.sergeyev@gmail.com>
2023-10-28 23:01:20 +0100

    Initial source code

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,55 @@
 <script lang="ts">
   import TailwindCss from "./lib/TailwindCSS.svelte";
-  import Welcome from "./components/Welcome.svelte";
+  import ItemList from "./components/ItemList.svelte";
+  import InputFields from "./components/InputFields.svelte";
+  import { onMount } from "svelte";
+
+  let items = [];
+  const newItems = [];
+
+  async function fetchItems() {
+    const response = await fetch(
+      "https://docs.google.com/spreadsheets/d/e/2PACX-1vTQ7tQz6gqyRWbc15gzLKZyFjVyNAkmmlLE4mGzlpyK6SY1piANAci7CaVcYlxmse8hcMzHo1W6QUDi/pub?output=csv"
+    );
+    const text = await response.text();
+    const lines = text.split("\n").slice(1);
+    items = lines.map((line) => {
+      const [name, price, quantity] = line.split(",");
+      return { name, price, quantity };
+    });
+  }
+
+  let search = "";
+
+  function addItem(newItem) {
+    items = [...items, newItem];
+    newItems.push(newItem);
+  }
+
+  function dumpItems() {
+    const data = newItems
+      .map((item) => `${item.name}\t${item.price}\t${item.quantity}`)
+      .join("\n");
+    navigator.clipboard.writeText(data).then(
+      () => alert("Data copied to clipboard!"),
+      (err) => alert("Failed to copy data!")
+    );
+  }
+
+  onMount(() => fetchItems());
 </script>
 
 <TailwindCss />
 
-<div class="flex h-full w-full">
-  <Welcome />
-  <!-- Components go here. -->
+<div class="flex flex-col items-center h-full w-full p-8 bg-gray-100">
+  <div class="w-full max-w-md bg-white p-6 rounded-lg shadow-md">
+    <InputFields {addItem} bind:search />
+    <ItemList {items} {search} />
+    <button
+      on:click={dumpItems}
+      class="mt-4 w-full bg-green-500 text-white p-2 rounded hover:bg-green-600"
+    >
+      Dump
+    </button>
+  </div>
 </div>
---
+++ src/components/InputFields.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+    export let addItem;
+    export let search: string;
+
+    let item = "";
+    let price: number | null = null;
+    let quantity = "1";
+
+    // Update the 'item' variable whenever 'search' changes
+    $: search = item;
+
+    function submit() {
+        addItem({ name: item, price, quantity });
+        item = "";
+        price = null;
+    }
+</script>
+
+<div class="flex flex-col mb-4 space-y-4">
+    <input
+        type="text"
+        placeholder="Item"
+        bind:value={item}
+        class="p-2 border rounded"
+    />
+    <input
+        type="number"
+        placeholder="Price"
+        bind:value={price}
+        class="p-2 border rounded"
+    />
+    <select bind:value={quantity} class="p-2 border rounded">
+        <option value="1">1</option>
+        <option value="10">10</option>
+    </select>
+    <button
+        on:click={submit}
+        class="bg-blue-500 text-white p-2 rounded hover:bg-blue-600"
+    >
+        +
+    </button>
+</div>
