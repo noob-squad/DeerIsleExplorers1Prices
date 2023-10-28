ea6109e02e41ab95e6354d53ac87a87d78f1fb8e
Volodymyr Sergeyev <volodymyr.sergeyev@gmail.com>
2023-10-28 23:52:55 +0100

    Adding the base

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,6 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 export default defineConfig({
   plugins: [svelte({
     /** Plugin options go here. */
-  })]
+  })],
+  base: '/DeerIsleExplorers1Prices/'
 })
